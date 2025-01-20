cef7cd4a79db381f3c56c66d499ab3e56ba4ced9
Saadaoui Oussama <oussama.saadaoui.dev@gmail.com>
2025-01-20 15:26:16 +0100

    Add missing emit events declaration to hide a bue warning

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -23,6 +23,7 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
 	name: 'SettingsComponent',
+	emits: ['close'],
 	props: {
 		visible: { type: Boolean, default: false },
 	},
