18638cb745d89a55484207455058b24e4114831d
Saadaoui Oussama <oussama.saadaoui.dev@gmail.com>
2025-02-13 00:07:18 +0100

    Change the way of defining preload channels

--- public/js/dashboard-preload.js
+++ public/js/dashboard-preload.js
@@ -1,25 +1,24 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-const ChannelConnector = (channel) => {
+const Channel = (name) => {
 	const callbacks = {};
-	return {
+	const channel = {
 		on: (event, callback) => {
 			if (callbacks[event]) ipcRenderer.off(event, callbacks[event]);
-			callbacks[event] = (_, key, ...data) => key === channel && callback(...data);
+			callbacks[event] = (_, key, ...data) => key === name && callback(...data);
 			ipcRenderer.on(event, callbacks[event]);
 		},
 		off: (event) => {
 			if (callbacks[event]) ipcRenderer.off(event, callbacks[event]);
 			delete callbacks[event];
 		},
-		send: (key, ...data) => ipcRenderer.send(`dashboard-${channel}`, key, ...data),
-		invoke: async (key, ...data) => await ipcRenderer.invoke(`dashboard-${channel}`, key, ...data),
+		send: (key, ...data) => ipcRenderer.send(`dashboard-${name}`, key, ...data),
+		invoke: async (key, ...data) => await ipcRenderer.invoke(`dashboard-${name}`, key, ...data),
 	};
+	return [name, channel];
 };
 
-contextBridge.exposeInMainWorld('NovaWallpaper', {
-	window: ChannelConnector('window'),
-	files: ChannelConnector('files'),
-	json: ChannelConnector('json'),
-	database: ChannelConnector('database'),
-});
+contextBridge.exposeInMainWorld(
+	'NovaWallpaper',
+	Object.fromEntries(['window', 'files', 'json', 'database'].map(Channel))
+);
