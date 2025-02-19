c08f7c455960d738c5bd3986f9d7923453ab9cf5
Saadaoui Oussama <oussama.saadaoui.dev@gmail.com>
2025-02-19 16:57:43 +0100

    Provide useDialog to all wallpapers as a helper function and make it easy to generate dialog modals for all users

--- public/js/renderer-preload.js
+++ public/js/renderer-preload.js
@@ -1,7 +1,164 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
+const useDialog = (() => {
+	const styles = {
+		container: {
+			position: 'absolute',
+			width: '100vw',
+			height: '100vh',
+			top: '0',
+			left: '0',
+			zIndex: '9999999999',
+			backgroundColor: '#00000000',
+			transition: 'background-color 0.3s ease-in-out',
+		},
+		visibleContainer: {
+			backgroundColor: '#00000060',
+		},
+		modal: {
+			position: 'fixed',
+			top: '57%',
+			left: '50%',
+			transform: 'translate(-50%, -50%)',
+			minWidth: '300px',
+			maxWidth: '85vw',
+			padding: '14px',
+			backgroundColor: 'var(--body-bg, #1e1e1e)',
+			border: '1px solid var(--border-color, #2e2e2e)',
+			borderRadius: '7px',
+			boxShadow: '0 4px 4px #00000080',
+			visibility: 'hidden',
+			opacity: '0',
+			transition: 'top 0.3s ease-in-out, opacity 0.3s ease-in-out',
+		},
+		visibleModal: {
+			top: '47%',
+			visibility: 'visible',
+			opacity: '1',
+		},
+		title: {
+			fontSize: '17px',
+			color: '#ffffff',
+			fontFamily: "system-ui, 'Trebuchet MS', 'Segoe UI', Helvetica, Arial, sans-serif",
+		},
+		buttons: ({ rtl }) => ({
+			marginTop: '14px',
+			display: 'flex',
+			justifyContent: 'flex-end',
+			gap: '14px',
+			flexDirection: rtl ? 'row-reverse' : 'row',
+		}),
+		button: ({ backgroundColor, textColor }) => ({
+			backgroundColor,
+			color: textColor,
+			display: 'block',
+			padding: '8px 20px',
+			border: 'none',
+			borderRadius: '7px',
+			cursor: 'pointer',
+			fontSize: '14px',
+			fontWeight: '500',
+			fontFamily: "system-ui, 'Trebuchet MS', 'Segoe UI', Helvetica, Arial, sans-serif",
+			transition: 'background-color 0.15s ease-in-out',
+		}),
+		buttonHover: ({ backgroundColorHover }) => ({
+			backgroundColor: backgroundColorHover,
+		}),
+	};
+
+	const buttons = {
+		neutralBtn: () => ({
+			backgroundColor: 'var(--neutral-color, #343434)',
+			backgroundColorHover: 'var(--neutral-hover, #444444)',
+			textColor: '#ffffff',
+		}),
+		cancelBtn: () => ({
+			backgroundColor: 'var(--danger-color, #a5352b)',
+			backgroundColorHover: 'var(--danger-color-hover, #bf2a1b)',
+			textColor: '#ffffff',
+		}),
+		primaryBtn: ({ danger }) => ({
+			text: 'Got it!',
+			backgroundColor: danger ? 'var(--danger-color, #a5352b)' : 'var(--primary-color, #007bff)',
+			backgroundColorHover: danger ? 'var(--danger-color-hover, #bf2a1b)' : 'var(--primary-color-active, #0074f0)',
+			textColor: '#ffffff',
+		}),
+	};
+
+	const setStyles = (target, source, selector, args) => {
+		const getStyles = (styles) =>
+			typeof styles === 'function' ? styles(args || {}) : typeof styles === 'object' ? styles : undefined;
+		Object.assign(target.style, {
+			...(styles && getStyles(styles[selector])),
+			...getStyles(source && source[selector]),
+		});
+	};
+
+	function createButton(button, value, styles, defaultButton, response) {
+		button = typeof button === 'string' ? { text: button } : button;
+		if (!button && value !== true) return { ref: 0, button: null };
+		button = { ...defaultButton(button || {}), ...button };
+		if (!button || !button.text || typeof button.text !== 'string') return { ref: 0, button: null };
+		const btn = document.createElement('button');
+		btn.addEventListener('click', () => response(value));
+		btn.textContent = button.text;
+		setStyles(btn, styles, 'button', button);
+		btn.addEventListener('mouseenter', () => setStyles(btn, styles, 'buttonHover', button));
+		btn.addEventListener('mouseleave', () => setStyles(btn, styles, 'button', button));
+		return { ref: 1, button: btn };
+	}
+
+	function useDialog(title, options) {
+		const { cancelBtn, neutralBtn, primaryBtn, closable, rtl, styles } = options;
+
+		return new Promise((resolve) => {
+			const container = document.createElement('div');
+			const modal = document.createElement('div');
+			const message = document.createElement('p');
+			message.innerHTML = typeof title === 'string' ? title : '';
+			const buttonsContainer = document.createElement('div');
+			const neutral = createButton(neutralBtn, undefined, styles, buttons.neutralBtn, (v) => response(v));
+			const cancel = createButton(cancelBtn, false, styles, buttons.cancelBtn, (v) => response(v));
+			const primary = createButton(primaryBtn, true, styles, buttons.primaryBtn, (v) => response(v));
+
+			if (closable !== false) {
+				modal.addEventListener('click', (event) => event.stopPropagation());
+				container.addEventListener('click', () => response(undefined));
+			}
+
+			if (neutral.button) buttonsContainer.appendChild(neutral.button);
+			if (cancel.button) buttonsContainer.appendChild(cancel.button);
+			if (primary.button) buttonsContainer.appendChild(primary.button);
+			modal.appendChild(message);
+			modal.appendChild(buttonsContainer);
+			container.appendChild(modal);
+			document.body.appendChild(container);
+
+			setStyles(message, styles, 'title');
+			setStyles(buttonsContainer, styles, 'buttons', { rtl: !!rtl, buttons: neutral.ref + cancel.ref + primary.ref });
+			setStyles(modal, styles, 'modal');
+			setStyles(container, styles, 'container');
+
+			setTimeout(() => {
+				setStyles(container, styles, 'visibleContainer');
+				setStyles(modal, styles, 'visibleModal');
+			}, 1);
+
+			function response(value) {
+				setStyles(container, styles, 'container');
+				setStyles(modal, styles, 'modal');
+				container.addEventListener('transitionend', () => container.remove());
+				resolve(value);
+			}
+		});
+	}
+
+	return useDialog;
+})();
+
 contextBridge.exposeInMainWorld('NovaWallpaper', {
-	readJson: (filename, isArray) => ipcRenderer.invoke(`renderer-json`, 'read', filename, isArray),
-	writeJson: (filename, data) => ipcRenderer.invoke(`renderer-json`, 'write', filename, data),
-	execute: (permissionId) => ipcRenderer.invoke(`renderer-execute`, 'execute', permissionId),
+	readJson: (filename, isArray) => ipcRenderer.invoke('renderer-json', 'read', filename, isArray),
+	writeJson: (filename, data) => ipcRenderer.invoke('renderer-json', 'write', filename, data),
+	execute: (permissionId) => ipcRenderer.invoke('renderer-execute', 'execute', permissionId),
+	dialog: (title, options) => useDialog(title, options),
 });
